32daa42efb39883ecff259c380179e54e53e03c5
Caio Silva <caiosantossp@gmail.com>
2023-06-01 02:56:27 -0300

    Create desafios_masterclass8.dart
    primeiro commit

---
+++ desafios_masterclass8.dart
@@ -0,0 +1,20 @@
+void main(List<String> arguments) {
+  print(imc(103.0, 1.85));
+  print(fibonacci(1000));
+}
+
+List fibonacci(int limite) {
+  List numeros = [];
+  for (int i = 0; i < limite; i++) {
+    int soma = i;
+    if (numeros.length > 1) {
+      soma = numeros[i - 1] + numeros[i - 2];
+    }
+    numeros.add(soma);
+  }
+  return numeros;
+}
+
+double imc(double peso, double altura) {
+  return peso / (altura * altura);
+}
